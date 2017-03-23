285838bf4dc97701c697ab87e64d988458c1fb70
wangtiantian <1106648114@qq.com>
2017-03-23 17:44:41 +0800

    1

---
+++ gulpfile.js
@@ -0,0 +1,131 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+
+// Set the banner content
+var banner = ['/*!\n',
+    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' */\n',
+    ''
+].join('');
+
+// Compile LESS files from /less into /css
+gulp.task('less', function() {
+    return gulp.src('less/*.less')
+        .pipe(less())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('less/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Minify compiled CSS
+gulp.task('minify-css', ['less'], function() {
+    return gulp.src('dist/css/sb-admin-2.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// scss编译后的所有css将注入到浏览器里实现更新
+gulp.task('sass', function() {
+    return gulp.src("sass/*.scss")
+        .pipe(sass())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// gulp.task('sass', function() {
+//     return gulp.src("sass/css.scss")
+//         .pipe(sass())
+//         .pipe(header(banner, { pkg: pkg }))
+//         .pipe(gulp.dest('dist/css'))
+//         .pipe(browserSync.reload({
+//             stream: true
+//         }))
+// });
+
+
+// Copy JS to dist
+gulp.task('js', function() {
+    return gulp.src(['js/*.js'])
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+})
+
+// Minify JS
+gulp.task('minify-js', ['js'], function() {
+    return gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('copyfonts', function() {
+     gulp.src('fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+})
+gulp.task('copyimages', function() {
+     gulp.src('images/**/*')
+        .pipe(gulp.dest('dist/images'))
+})
+
+// Copy vendor libraries from /bower_components into /vendor
+gulp.task('copy', function() {
+    gulp.src(['bower_components/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest('vendor/bootstrap'))
+
+    gulp.src(['bower_components/bootstrap-social/*.css', 'bower_components/bootstrap-social/*.less', 'bower_components/bootstrap-social/*.scss'])
+        .pipe(gulp.dest('vendor/bootstrap-social'))
+
+    gulp.src(['bower_components/font-awesome/**/*', '!bower_components/font-awesome/*.json', '!bower_components/font-awesome/.*'])
+        .pipe(gulp.dest('vendor/font-awesome'))
+
+    gulp.src(['bower_components/jquery/dist/jquery.js', 'bower_components/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('vendor/jquery'))
+})
+
+// Run everything
+gulp.task('default', ['minify-css', 'minify-js', 'copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'less', 'sass','minify-css', 'js', 'minify-js'], function() {
+    gulp.watch('less/*.less', ['less']);
+    gulp.watch('sass/*.scss', ['sass']);
+    gulp.watch('dist/css/*.css', ['minify-css']);
+    gulp.watch('js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('pages/*.html', browserSync.reload);
+    gulp.watch('dist/js/*.js', browserSync.reload);
+});
